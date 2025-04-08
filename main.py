603538cba70931e5871d02282976db1a9d59aa11
maciej-pazdzior <maciek.pazdzior@gmail.com>
2025-04-08 19:14:31 +0200

    drawing line

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from window import Window
+from graphics import Window, Point, Line
+
 
 def main():
     win = Window(800, 600)
+    point1 = Point(1, 2)
+    point2 = Point(100, 200)
+    line = Line(point1, point2)
+    win.draw_line(line, "red")
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
